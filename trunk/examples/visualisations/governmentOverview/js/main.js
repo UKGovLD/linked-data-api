a933c6fbb12b360c9c3ce070dd0951db338bc5f8
jeni.tennison <jeni.tennison@1a4224c4-e8f6-11de-95aa-47033305a456>
2010-11-12 11:09:03 +0000

    Initial commit of Dan Paul Smith's visualisations used over reference.data.gov.uk linked data API

---
+++ trunk/examples/visualisations/governmentOverview/js/main.js
@@ -0,0 +1,47 @@
+
+
+$(document).ready(function() {
+
+	$("#infovis").width($(window).width()-100);
+	$("#infovis").height($(window).height()-36);
+	
+	//$("#log").corner();
+	
+	$("div#right ul li").hover(function() {
+		$(this).children().filter("div").show();
+	},function() {
+		$(this).children().filter("div").hide();		
+	});
+	
+	//$("select#levels").val("2");
+	//$("input[name='deputies']").val("No");
+	
+	$("select#levels").val("2");
+	
+	$("select#levels").change(function() {
+		global_TM.canvas.opt.levelsToShow = parseInt($(this).val());
+		//lvlsToShow = parseInt($(this).val());
+		//global_TM.canvas.clear();
+		//$("#infovis").html("");
+		//global_TM.loadJSON(global_govJSON);  
+		global_TM.refresh(); 
+	});
+
+	$("input[name='deputies']").val("No");
+
+	$("input[name='deputies']").change(function(){
+		if($(this).val() == "Yes") {
+			global_TM.canvas.clear();
+			$("#infovis").html("");
+			includeDeputyDirectors = true;
+			init();
+    	} else if ($(this).val() == 'No') {
+			global_TM.canvas.clear();
+			$("#infovis").html("");
+			includeDeputyDirectors = false;
+			init();
+   		}
+	});
+
+		
+});
