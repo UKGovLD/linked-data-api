00e375a428b1f376078c285abcec312d7861f79e
danpaulsmith@gmail.com <danpaulsmith@gmail.com@1a4224c4-e8f6-11de-95aa-47033305a456>
2010-11-15 02:49:48 +0000

    - organogram stops moving on node click - overview; departments can be sorted by number of posts - overview; posts are sized by number of reportsTo's - using reference.gov.uk API now

--- examples/visualisations/governmentOverview/js/main.js
+++ examples/visualisations/governmentOverview/js/main.js
@@ -44,7 +44,21 @@ $(document).ready(function() {
    		}
 	});
 
-		
+	$("input[name='resizeBy']").change(function(){
+		if($(this).val() == "Unit") {
+			global_TM.canvas.clear();
+			$("#infovis").html("");
+			sizeByUnits = true;
+			sizeByPosts = false;
+			init();
+    	} else if ($(this).val() == "Post") {
+			global_TM.canvas.clear();
+			$("#infovis").html("");
+			sizeByUnits = false;
+			sizeByPosts = true;
+			init();
+   		}
+	});		
 });
 
 function resetSourceLinks() {
@@ -59,4 +73,32 @@ function resetSourceLinks() {
 	});
 		
 	return false;
+}
+
+/**
+ * (C) 2007 Andrew Gwozdziewycz <apgwoz@gmail.com>
+ * VERSION: 0.1, Released under an MIT License. 
+ * URL: http://www.apgwoz.com/textfit/
+ **/
+function textFit(node, minfs, maxfs, width, height) {
+    var fits = function(sz) {
+        node.style.fontSize = sz + 'px';
+        return node.offsetWidth <= width && node.offsetHeight <= height;
+    };
+    var hfs = maxfs - minfs;
+    var tst = minfs + Math.ceil(hfs / 2);
+    var opsz = node.style.fontSize;
+    
+    while ((hfs/2) >= 1) {
+        if (fits(tst)) {
+            minfs = tst;
+        }
+        else {
+            maxfs = tst;
+        }
+        hfs = maxfs - minfs;
+        tst = minfs + Math.ceil(hfs/2);
+    }
+    opsz = fits(maxfs) ? maxfs: minfs;
+    node.style.fontSize = opsz + 'px';
 }
