e509784906e27a9ba543242e8cdfc2d6a2958666
danpaulsmith@gmail.com <danpaulsmith@gmail.com@1a4224c4-e8f6-11de-95aa-47033305a456>
2010-11-15 17:39:10 +0000

    Fixed text & tooltip for overview

--- trunk/examples/visualisations/governmentOverview/js/main.js
+++ trunk/examples/visualisations/governmentOverview/js/main.js
@@ -102,3 +102,29 @@ function textFit(node, minfs, maxfs, width, height) {
     opsz = fits(maxfs) ? maxfs: minfs;
     node.style.fontSize = opsz + 'px';
 }
+
+(function ($) {
+$.fn.vAlign = function() {
+	return this.each(function(i){
+	var h = $(this).height();
+	var oh = $(this).outerHeight();
+	var mt = (h + (oh - h)) / 2;	
+	$(this).css("margin-top", "-" + mt + "px");	
+	$(this).css("top", "50%");
+	$(this).css("position", "absolute");	
+	});	
+};
+})(jQuery);
+
+(function ($) {
+$.fn.hAlign = function() {
+	return this.each(function(i){
+	var w = $(this).width();
+	var ow = $(this).outerWidth();	
+	var ml = (w + (ow - w)) / 2;	
+	$(this).css("margin-left", "-" + ml + "px");
+	$(this).css("left", "50%");
+	$(this).css("position", "absolute");
+	});
+};
+})(jQuery);
