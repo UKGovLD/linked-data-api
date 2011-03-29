afe8014f5b250493617567401c56353959d2e8dc
danpaulsmith@gmail.com <danpaulsmith@gmail.com@1a4224c4-e8f6-11de-95aa-47033305a456>
2011-03-29 14:02:01 +0000

    - Fixing IE7 styles and bugs for post-list

--- examples/visualisations/gov-structure/scripts/post-list.js
+++ examples/visualisations/gov-structure/scripts/post-list.js
@@ -9,6 +9,318 @@ visualisation.
 
 */
 
+var PostList = {
+	vars:{
+		dept:"",
+		unit:"",
+		postType:"",
+		apiCallInfo:{
+			postTypeList:{}
+		}
+	},
+	init:function(pDept,pUnit,pType){
+	
+		PostList.vars.dept = pDept;
+		PostList.vars.unit = pUnit;
+		PostList.vars.postType = pType;
+		
+		if(PostList.vars.postType < 1){
+			PostList.vars.postType = $("select#postType").val();
+		} else {
+			$("select#postType").val(PostList.vars.postType.replace("+"," "));
+		}
+
+		$('div#apiCalls').hide();
+		$("a.source").remove();
+		$('div.apiCall').remove();
+		
+		PostList.getPostListData();
+		
+	},
+	getPostListData:function(){
+
+		PostList.showLog("Loading data ...");
+
+		PostList.vars.apiCallInfo.postTypeList = {
+			title:"Retrieve Permanent Secretary posts within department",
+			description:"Asks for a list of all Permanent Secretary posts within a department.",
+			url:"http://reference.data.gov.uk/doc/department/"+PostList.vars.dept+"/post",
+			parameters:"?_pageSize=100&type.label="+PostList.vars.postType.replace(" ","+")
+		};
+		
+		$.ajax({
+			url: PostList.vars.apiCallInfo.postTypeList.url+".json"+PostList.vars.apiCallInfo.postTypeList.parameters+"&callback=?",
+			type: "GET",
+			dataType: "jsonp",
+			async:true,
+			success: function(json){	
+				PostList.loadDepts(json);
+			},
+			error: function(){
+				PostList.changeLog("Error loading data",false);
+			}
+		});	
+	},
+	loadDepts:function(json){
+	
+		//var postList = new Array();
+			
+		var html='', slug, label, name, comment, email, phone, unit, dept, type, reportsTo;
+					
+		for(var i=0,itemLength=json.result.items.length;i<itemLength;i++){
+		
+			slug = json.result.items[i]._about.split("/");
+			slug = slug[slug.length-1];
+			
+			html += '<div class="post" rel="'+slug+'" data-id="id-'+slug+'">';
+						
+			try{
+				label = json.result.items[i].label[0];
+			}catch(e){}
+			if(typeof label != 'undefined'){
+				html += '<h3 class="label" data-type="title">'+label+'</h3>';
+			} else {
+				html += '<h3 class="label" data-type="title">?</h3>';						
+			}			
+						
+			html += '<div class="image"><img width="130" src="../images/no_profile.jpg" /></div>';
+			
+			try{
+				name = json.result.items[i].heldBy[0].name;
+			}catch(e){}
+			if(typeof name != 'undefined'){
+				html += '<p class="name ui-state-hover" data-type="name">'+name+'</p>';
+			} else {
+				html += '<p class="name ui-state-hover" data-type="name">?</p>';							
+			}
+
+			try{
+				comment = json.result.items[i].comment;		
+			}catch(e){}
+			if(typeof comment != 'undefined'){
+				//html += '<p class="comment">'+comment+'</p>';
+			} else {
+			}
+
+			html += '<table>';
+			
+
+			try{
+				email = json.result.items[i].heldBy[0].email.label[0];
+			}catch(e){}
+			if(typeof email != 'undefined'){
+				//html += '<tr class="email odd"><td class="label">Email</td><td>'+email+'</td></tr>';
+			} else {
+				//html += '<tr class="email odd"><td class="label">Email</td><td>'+email+'</td></tr>';
+			}			
+
+			try{
+				phone = json.result.items[i].heldBy[0].phone.label[0];
+			}catch(e){}
+			if(typeof phone != 'undefined'){
+				//html += '<tr class="phone even"><td class="label">Phone</td><td>'+phone+'</td></tr>';
+			} else {
+				//html += '<tr class="phone even"><td class="label">Phone</td><td>'+phone+'</td></tr>';
+			}	
+						
+
+			try{
+				unit = json.result.items[i].postIn[0].label[0];
+			}catch(e){}
+			if(typeof unit != 'undefined'){
+				html += '<tr class="unit odd"><td class="label">Unit</td><td data-type="unit">'+unit+'</td></tr>';
+			} else {
+				html += '<tr class="unit odd"><td class="label">Unit</td><td data-type="unit">?</td></tr>';
+			}	
+			
+			try{
+				dept = json.result.items[i].postIn[1].label[0];
+			}catch(e){}
+			if(typeof dept != 'undefined'){
+				//html += '<tr class="dept even"><td class="label">Department</td><td data-type="dept">'+dept+'</td></tr>';
+			} else {
+				//html += '<tr class="dept even"><td class="label">Department</td><td data-type="dept">'+dept+'</td></tr>';
+			}	
+			
+			/*
+			try{
+				type = json.result.items[i].type[1].label[0];
+			}catch(e){}
+			if(typeof type != 'undefined'){
+			} else {
+				html += '<tr class="type odd"><td class="label">Type</td><td>?</td></tr>';
+			}	
+			*/
+			html += '<tr class="type odd"><td class="label">Type</td><td>'+PostList.vars.postType+'</td></tr>';
+		
+			try{
+				reportsTo = json.result.items[i].reportsTo[0].heldBy[0].name;
+			}catch(e){}
+			if(typeof reportsTo != 'undefined'){
+				html += '<tr class="reportsTo even end"><td class="label">Reports to</td><td>'+reportsTo+'</td></tr>';
+			} else {
+			}	
+			
+			html += '</table>';
+			
+			html += '</div>';
+		}
+		
+		// List of div's generated
+		// Call QuickSand to swap with original divs.
+		
+		//$('#infovis').quicksand( html, { adjustHeight: 'dynamic' } );
+		
+		$("div.postHolder").html(html);
+		
+		$("div.post").click(function(){
+			window.location = "../organogram?dept="+PostList.vars.dept+"&post="+$(this).attr("rel");
+		});
+		
+		//$("div.post").dropShadow();
+		
+		$("#infovis").css("visibility","visible");
+		
+		PostList.setQuickSand();
+								
+		PostList.displayDataSources();
+		
+		PostList.hideLog();
+
+	},
+	setQuickSand:function(){
+	
+	  // bind radiobuttons in the form
+	  var $sortInput = $('select#sortBy');
+	
+	  // get the first collection
+	  var $posts = $('div.postHolder');
+	  
+	  // clone applications to get a second collection
+	  var $data = $posts.clone();
+	
+	  // attempt to call Quicksand on every form change
+	  $sortInput.change(function(e) {
+			
+	    // if sorted by size
+	    if ($(this).val() == "name") {
+	    	$sortedData = $data.find("div.post").sort(function(a, b){
+		  		return $(a).find('p[data-type=name]').text().toLowerCase() > $(b).find('p[data-type=name]').text().toLowerCase() ? 1 : -1;
+	  		});
+	    } else if ($(this).val() == "title") {
+	
+	    	$sortedData = $data.find("div.post").sort(function(a, b){
+		  		return $(a).find('h3[data-type=title]').text().toLowerCase() > $(b).find('h3[data-type=title]').text().toLowerCase() ? 1 : -1;
+	  		});
+	      
+	    } else if ($(this).val() == "unit") {
+	    	$sortedData = $data.find("div.post").sort(function(a, b){
+		  		return $(a).find('td[data-type=unit]').text().toLowerCase() > $(b).find('td[data-type=unit]').text().toLowerCase() ? 1 : -1;
+	  		});
+	    }   
+		
+	    // finally, call quicksand
+	    $posts.quicksand($sortedData,{
+	    	adjustHeight: 'dynamic'
+	    },function(){
+			$("div.post").click(function(){
+					window.location = "../organogram?dept="+PostList.vars.dept+"&post="+$(this).attr("rel");
+				});
+	    });
+	
+	  });
+	
+	},	
+	displayDataSources:function() {
+		/*
+		var html='<p class="label">Data sources</p>';
+		
+		for(var i=0;i<api_call_info.length;i++){
+			
+			html += '<a class="source">'+(i+1)+'</a>';
+			
+			html += '<div class="apiCall shadowBox">';
+			
+			html += '<p class="title"><span>API call '+(i+1)+':</span>'+api_call_info[i].title+'</p>';
+			html += '<p class="description"><span>Description:</span>'+api_call_info[i].description+'</p>';
+			html += '<p class="url"><span>URL:</span><a href="'+api_call_info[i].url+'.html" target="_blank">'+api_call_info[i].url+'</a></p>';	
+			
+			html += '<p class="params"><span>Parameters:</span></p>';
+			
+			var tempParams = api_call_info[i].parameters.replace("?","").split("&");
+			
+			html += '<ul class="paramlist">';
+			for(var j=0;j<tempParams.length;j++){
+				html+= '<li>'+tempParams[j]+'</li>';
+			}
+			html += '</ul>';
+			
+			html += '<p class="formats"><span>Formats:</span>';
+			html += '<a href="'+api_call_info[i].url+'.rdf'+api_call_info[i].parameters+'" target="_blank">RDF</a>';
+			html += '<a href="'+api_call_info[i].url+'.ttl'+api_call_info[i].parameters+'" target="_blank">TTL</a>';
+			html += '<a href="'+api_call_info[i].url+'.xml'+api_call_info[i].parameters+'" target="_blank">XML</a>';
+			html += '<a href="'+api_call_info[i].url+'.json'+api_call_info[i].parameters+'" target="_blank">JSON</a>';
+			html += '<a href="'+api_call_info[i].url+'.html'+api_call_info[i].parameters+'" target="_blank">HTML</a>';
+			html += '</p>';
+			html += '<a class="close">x</a>';
+			html += '</div><!-- end apiCall -->';
+			
+		}
+		
+		//$('div#apiCalls').html($('div#apiCalls').html()+html);
+		$('div#apiCalls').html(html);
+		
+		$('div#apiCalls a.source').each(function(){
+			$(this).button({text:true}).toggle(function() { $(this).next('div.apiCall').show();return false; },function(){$('div.apiCall').hide();return false;});
+		});
+		
+		$('p.formats a').each(function(){
+			$(this).button({text:true});
+		});
+		
+		resetSourceLinks();
+		
+		$('div#apiCalls').fadeIn();
+			
+		return false;
+		*/
+	},
+	resetSourceLinks:function() {
+		
+		$("div#apiCalls a.source").click(function(){
+			$("div#apiCalls div.apiCall").hide();
+			$(this).next().fadeIn();
+		});
+		
+		$("a.close").click(function(){
+			$(this).parent().fadeOut();
+		});
+			
+		return false;
+	},	
+ 	showLog:function(string){
+		$("#log img").show();
+		$("#log span").html(string);
+		$("#log").fadeIn();
+		return false;
+	},
+	changeLog:function(string, showImg){
+		$("#log span").html(string);
+		if(showImg){
+	
+		}else {
+			$("#log img").hide();
+		}
+		return false;
+	},
+	hideLog:function() {
+		setTimeout(function() {
+			$("#log").fadeOut();
+		},1000);
+		return false;	
+	}		
+}; // end PostList
+
 
 $(document).ready(function() {
 
@@ -34,265 +346,17 @@ $(document).ready(function() {
 	
 	$("select#postType").change(function(){
 		$("div.postHolder").html("");
-		$("div.postHolder").height(600);
-		init(param_dept,param_unit,$(this).val());
+		//$("div.postHolder").css("height","auto");
+		PostList.init(PostList.vars.dept,PostList.vars.unit,$(this).val());
 		//$('#infovis').quicksand( $('#infovis').find("div."+postType), { adjustHeight: 'dynamic' } );
 	});
 	
 	//$("select#sortBy").val("name");
 		
-	init(param_dept,param_unit,$("select#postType").val());
-	
 	$("div#right").show();
+	
+	if($.browser.msie) {
+		$("div#log").corner();
+		$("div#right").corner("tl bl 10px");
+	}	
 });
-
-var api_call_info;
-
-function init(pDept,pUnit,postType){
-	
-	deptParam = pDept;
-	unitParam = pUnit;
-	
-	api_call_info = [];
-	$('div#apiCalls').hide();
-	$("a.source").remove();
-	$('div.apiCall').remove();
-		
-	loadDepts(deptParam,unitParam,postType);
-	
-}
-
-/*
- *
- */
-function loadDepts(pDept,pUnit,postType) {
-	
-	showLog("Loading data ...");
-
-	var postList = new Array();
-	
-	// Description of API call
-	api_call_info.push({
-		title:"Retrieve Permanent Secretary posts within department",
-		description:"Asks for a list of all Permanent Secretary posts within a department.",
-		url:"http://reference.data.gov.uk/doc/department/"+pDept+"/post",
-		parameters:"?_pageSize=100&type.label="+postType.replace(" ","+")
-	});	
-	
-	//api_call_info[0].url = "http://danpaulsmith.com/puelia3/doc/department.json?_view=minimal&_properties=unit.label,unit.post.label,unit.post.reportsTo&_pageSize=100"
-	//alert("using danpaulsmith.com API");
-	
-	$.ajax({
-		url: api_call_info[0].url+".json"+api_call_info[0].parameters+"&callback=?",
-		type: "GET",
-		dataType: "jsonp",
-		async:true,
-		success: function(json){
-			
-			var html="";
-						
-			for(var i=0;i<json.result.items.length;i++){
-			
-				var slug = json.result.items[i]._about.split("/");
-				slug = slug[slug.length-1];
-				
-				html += '<div class="post" rel="'+slug+'" data-id="id-'+slug+'">';
-				
-				var label = json.result.items[i].label[0];
-				html += '<h3 class="label" data-type="title">'+label+'</h3>';
-				html += '<div class="image"><img width="130" src="../images/no_profile.jpg" /></div>';
-
-				var name = json.result.items[i].heldBy[0].name;
-				if(typeof name != 'undefined'){
-					html += '<p class="name ui-state-hover" data-type="name">'+name+'</p>';
-				} else {
-					html += '<p class="name ui-state-hover" data-type="name">?</p>';				
-				}
-				html += '<p class="comment">'+comment+'</p>';
-
-				html += '<table>';
-
-				if(typeof json.result.items[i].heldBy[0].email != 'undefined'){
-					var email = json.result.items[i].heldBy[0].email.label[0];
-					//html += '<tr class="email odd"><td class="label">Email</td><td>'+email+'</td></tr>';
-				}
-				if(typeof json.result.items[i].heldBy[0].phone != 'undefined'){
-					var phone = json.result.items[i].heldBy[0].phone.label[0];
-					//html += '<tr class="phone even"><td class="label">Phone</td><td>'+phone+'</td></tr>';
-				}
-				var unit = json.result.items[i].postIn[0].label[0]; // handle
-				html += '<tr class="unit odd"><td class="label">Unit</td><td data-type="unit">'+unit+'</td></tr>';
-
-				var dept = json.result.items[i].postIn[1].label[0]; // handle
-				html += '<tr class="dept even"><td class="label">Department</td><td data-type="dept">'+dept+'</td></tr>';
-
-				try{
-					var type = json.result.items[i].type[0].label[0];
-					html += '<tr class="type odd"><td class="label">Type</td><td>'+type+'</td></tr>';
-				}catch(e){}
-				
-				var comment = json.result.items[i].comment;
-				
-				try{
-					var reportsTo = json.result.items[i].reportsTo[0].heldBy[0].name;
-					html += '<tr class="reportsTo even end"><td class="label">Reports to</td><td>'+reportsTo+'</td></tr>';
-				}catch(e){}			
-				
-				html += '</table>';
-				
-				html += '</div>';
-			}
-			
-			// List of div's generated
-			// Call QuickSand to swap with original divs.
-			
-			//$('#infovis').quicksand( html, { adjustHeight: 'dynamic' } );
-			
-			$("div.postHolder").html(html);
-			
-			$("div.post").click(function(){
-				window.location = "../organogram?dept="+pDept+"&post="+$(this).attr("rel");
-			});
-			
-			//$("div.post").dropShadow();
-			
-			$("#infovis").css("visibility","visible");
-			
-			setQuickSand();
-									
-			displayDataSources();
-			
-			hideLog();
-		}
-	});
-}
-
-
-function displayDataSources() {
-	
-	var html='<p class="label">Data sources</p>';
-	
-	for(var i=0;i<api_call_info.length;i++){
-		
-		html += '<a class="source">'+(i+1)+'</a>';
-		
-		html += '<div class="apiCall shadowBox">';
-		
-		html += '<p class="title"><span>API call '+(i+1)+':</span>'+api_call_info[i].title+'</p>';
-		html += '<p class="description"><span>Description:</span>'+api_call_info[i].description+'</p>';
-		html += '<p class="url"><span>URL:</span><a href="'+api_call_info[i].url+'.html" target="_blank">'+api_call_info[i].url+'</a></p>';	
-		
-		html += '<p class="params"><span>Parameters:</span></p>';
-		
-		var tempParams = api_call_info[i].parameters.replace("?","").split("&");
-		
-		html += '<ul class="paramlist">';
-		for(var j=0;j<tempParams.length;j++){
-			html+= '<li>'+tempParams[j]+'</li>';
-		}
-		html += '</ul>';
-		
-		html += '<p class="formats"><span>Formats:</span>';
-		html += '<a href="'+api_call_info[i].url+'.rdf'+api_call_info[i].parameters+'" target="_blank">RDF</a>';
-		html += '<a href="'+api_call_info[i].url+'.ttl'+api_call_info[i].parameters+'" target="_blank">TTL</a>';
-		html += '<a href="'+api_call_info[i].url+'.xml'+api_call_info[i].parameters+'" target="_blank">XML</a>';
-		html += '<a href="'+api_call_info[i].url+'.json'+api_call_info[i].parameters+'" target="_blank">JSON</a>';
-		html += '<a href="'+api_call_info[i].url+'.html'+api_call_info[i].parameters+'" target="_blank">HTML</a>';
-		html += '</p>';
-		html += '<a class="close">x</a>';
-		html += '</div><!-- end apiCall -->';
-		
-	}
-	
-	//$('div#apiCalls').html($('div#apiCalls').html()+html);
-	$('div#apiCalls').html(html);
-	
-	$('div#apiCalls a.source').each(function(){
-		$(this).button({text:true}).toggle(function() { $(this).next('div.apiCall').show();return false; },function(){$('div.apiCall').hide();return false;});
-	});
-	
-	$('p.formats a').each(function(){
-		$(this).button({text:true});
-	});
-	
-	resetSourceLinks();
-	
-	$('div#apiCalls').fadeIn();
-		
-	return false;
-}
-
-function showLog(string){
-	$("#log img").show();
-	$("#log span").html(string);
-	$("#log").fadeIn();
-	return false;
-}
-function changeLog(string, showImg){
-	$("#log span").html(string);
-	if(showImg){
-
-	}else {
-		$("#log img").hide();
-	}
-	return false;
-}
-function hideLog() {
-	setTimeout(function() {
-		$("#log").fadeOut();
-	},1000);
-	return false;	
-}
-function resetSourceLinks() {
-	
-	$("div#apiCalls a.source").click(function(){
-		$("div#apiCalls div.apiCall").hide();
-		$(this).next().fadeIn();
-	});
-	
-	$("a.close").click(function(){
-		$(this).parent().fadeOut();
-	});
-		
-	return false;
-}
-
-function setQuickSand(){
-
-  // bind radiobuttons in the form
-  var $sortInput = $('select#sortBy');
-
-  // get the first collection
-  var $posts = $('div.postHolder');
-  
-  // clone applications to get a second collection
-  var $data = $posts.clone();
-
-  // attempt to call Quicksand on every form change
-  $sortInput.change(function(e) {
-		
-    // if sorted by size
-    if ($(this).val() == "name") {
-    	$sortedData = $data.find("div.post").sort(function(a, b){
-	  		return $(a).find('p[data-type=name]').text().toLowerCase() > $(b).find('p[data-type=name]').text().toLowerCase() ? 1 : -1;
-  		});
-    } else if ($(this).val() == "title") {
-
-    	$sortedData = $data.find("div.post").sort(function(a, b){
-	  		return $(a).find('h3[data-type=title]').text().toLowerCase() > $(b).find('h3[data-type=title]').text().toLowerCase() ? 1 : -1;
-  		});
-      
-    } else if ($(this).val() == "unit") {
-    	$sortedData = $data.find("div.post").sort(function(a, b){
-	  		return $(a).find('td[data-type=unit]').text().toLowerCase() > $(b).find('td[data-type=unit]').text().toLowerCase() ? 1 : -1;
-  		});
-    }   
-	
-    // finally, call quicksand
-    $posts.quicksand($sortedData,{
-    	adjustHeight: 'dynamic'
-    });
-
-  });
-
-}
