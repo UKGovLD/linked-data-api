55b03db78f56c6e7497e5402bc460d8765a61b31
danpaulsmith@gmail.com <danpaulsmith@gmail.com@1a4224c4-e8f6-11de-95aa-47033305a456>
2011-04-09 12:15:46 +0000

    - Adding upload page content

---
+++ branches/organogram-2011-03-31/examples/visualisations/gov-structure/upload/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Organogram Data Upload | data.gov.uk</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+<link type="text/css" href="../css/upload.css" rel="stylesheet">
+
+<script language="javascript" type="text/javascript" src="../scripts/jquery-1.4.4.min.js"></script>
+<script language="javascript" type="text/javascript" src="../scripts/upload.js"></script>
+
+
+</head>
+<body class="dps">
+<div id="page">
+
+
+
+    <div role="banner" id="header">
+      <div id="login">
+      <img class="sprite" id="logo_hm-government" alt="HM Government logo" src="../images/1x1.gif">
+     </div>
+    <div class="inner">
+   
+   
+	  	  <a title="Back to homepage" accesskey="1" class="sprite" id="logo_datagovuk" href="/"><span>Back to homepage</span></a>
+	  	  
+	  <span class="sprite" id="slogan"><span>Opening up government data</span></span>
+
+    </div>
+    </div> <!-- /#header-inner, /#header -->
+    
+    <div id="main">
+                
+        <div role="main" id="content">
+  
+  		
+ 		<div class="steps">
+	 		<div class="step1">
+	 		<div class="image"><img src="../images/upload_file.png" width="90" height="90" /></div>
+	 		<div class="text"><p>Upload your spreadsheet</p></div>
+	 		</div>
+	 		<div class="step2">
+	 		<div class="image"><img src="../images/preview.png" width="90" height="90"  /></div>
+	 		<div class="text"><p>Preview organogram</p></div>	 		
+	 		</div>
+	 		<div class="step3">
+	 		<div class="image"><img src="../images/download_data.png" width="90" height="90"  /></div>
+	 		<div class="text"><p>Download data</p></div>	 		
+	 		</div>
+	  		<div class="clear"><!-- --></div>
+ 		</div> 
+  
+  
+          <div id="content-header">
+                                <h1 class="title">Upload your spreadsheet</h1>
+          </div> <!-- /#content-header -->
+                        
+          <div id="content-area">
+            <div class="node-inner">
+
+  <div class="content">
+  
+	<div class="upload panel">
+		<form id="upload_spreadsheet">
+				<fieldset>
+					<label>Title</label>
+					<input type="text" />
+					<label>Author</label>
+					<input type="text" />
+				 	<label>File</label>
+				 	<input type="file" /> 
+				 	<input type="submit" value="Upload" />
+				 	</fieldset>
+			</legend>
+		</form>
+	  </div> <!-- end upload panel -->
+	  
+	  <div class="preview panel">
+	  	<p></p>
+	  </div> <!-- end preview panel -->
+	  
+	  <div class="download panel">
+	   
+	  </div> <!-- end download panel -->
+	  
+	  </div>
+
+ 
+  
+</div> <!-- /node-inner, /node -->
+          </div>
+  
+            
+                    
+           
+  
+        </div> <!-- /#content -->
+                 
+               
+    </div> <!-- /#main -->
+
+          <div id="footer">
+</div><!-- footer -->
+
+
+</body>
+</html>
