807ae384cab3dbd28e0d5876bc014f1d12aaecc0
jeni.tennison <jeni.tennison@1a4224c4-e8f6-11de-95aa-47033305a456>
2011-04-10 21:53:53 +0000

    Fix for bug caused by including salary range

--- examples/visualisations/gov-structure/scripts/organogram.js
+++ examples/visualisations/gov-structure/scripts/organogram.js
@@ -886,73 +886,71 @@ var Orgvis = {
 						postID = postID[postID.length-1];
 						log("postID: "+postID);
 
-            if (Orgvis.vars.postList[postID] != 'undefined') {
-  						log("Orgvis.vars.postList[postID].children.length:");
-  						log(Orgvis.vars.postList[postID].children.length);
+						log("Orgvis.vars.postList[postID].children.length:");
+						log(Orgvis.vars.postList[postID].children.length);
+												
+						// Remove the empty junior staff node from the post that now has junior staff
+						var postChildren = Orgvis.vars.postList[postID].children;
+						
+						for(var j in postChildren){
+							if(postChildren[j].name == "No Junior Posts"){
+								log("Removing node: "+postChildren[j].name);
+								postChildren.splice(j,1);
+							}
+						}
 
-  						// Remove the empty junior staff node from the post that now has junior staff
-  						var postChildren = Orgvis.vars.postList[postID].children;
-
-  						for(var j in postChildren){
-  							if(postChildren[j].name == "No Junior Posts"){
-  								log("Removing node: "+postChildren[j].name);
-  								postChildren.splice(j,1);
-  							}
-  						}
-
-  						log("Orgvis.vars.postList[postID].children.length:");
-  						log(Orgvis.vars.postList[postID].children.length);						
-
-  						var addJPNode = true;
-
-  						for(var m in postChildren){
-  							log("searching postChildren fo JPNode:");
-  							log(postChildren[m]);
-  							if(postChildren[m].name == "Junior Posts"){
-  								log("Post already has Junior Post connected");
-  								addJPNode = false;
-  							}
-  						}
-
-  						if(addJPNode){
-  							// Add the 'Junior Posts' node to the post that holds the junior staff
-  							postChildren.push(Orgvis.makeJuniorPostNode());
-  							log("Added juniorPostsNode");
-  						}
-
-  						log("Orgvis.vars.postList[postID].children.length:");
-  						log(Orgvis.vars.postList[postID].children.length);
-
-  						log(postChildren);
-
-  						// Loop through the posts children
-  						for(var k in postChildren){
-
-  							log("postChildren[k].name:");
-  							log(postChildren[k].name);
-
-  							// If one of the posts's children is named "Junior Posts'
-  							if(postChildren[k].name == "Junior Posts"){
-
-  								log("Found the post's Junior Posts node:")
-  								log(postChildren[k]);
-
-  								// Add the actual junior staff item to the Junior Posts node
-  								postChildren[k].children.push(Orgvis.makeJuniorNode(el));
-
-  								log("el.fullTimeEquivalent:");
-  								log(el.fullTimeEquivalent);
-
-  								postChildren[k].data.fteTotal += el.fullTimeEquivalent;
-
-  								log("postChildren[k].data.fteTotal:");
-  								log(postChildren[k].data.fteTotal);
-
-  								log("Added a junior post");
-  								log(Orgvis.vars.postList[postID]);
-  							}
-  						}
-            }
+						log("Orgvis.vars.postList[postID].children.length:");
+						log(Orgvis.vars.postList[postID].children.length);						
+						
+						var addJPNode = true;
+						
+						for(var m in postChildren){
+							log("searching postChildren fo JPNode:");
+							log(postChildren[m]);
+							if(postChildren[m].name == "Junior Posts"){
+								log("Post already has Junior Post connected");
+								addJPNode = false;
+							}
+						}
+						
+						if(addJPNode){
+							// Add the 'Junior Posts' node to the post that holds the junior staff
+							postChildren.push(Orgvis.makeJuniorPostNode());
+							log("Added juniorPostsNode");
+						}
+						
+						log("Orgvis.vars.postList[postID].children.length:");
+						log(Orgvis.vars.postList[postID].children.length);
+						
+						log(postChildren);
+						
+						// Loop through the posts children
+						for(var k in postChildren){
+							
+							log("postChildren[k].name:");
+							log(postChildren[k].name);
+							
+							// If one of the posts's children is named "Junior Posts'
+							if(postChildren[k].name == "Junior Posts"){
+								
+								log("Found the post's Junior Posts node:")
+								log(postChildren[k]);
+								
+								// Add the actual junior staff item to the Junior Posts node
+								postChildren[k].children.push(Orgvis.makeJuniorNode(el));
+								
+								log("el.fullTimeEquivalent:");
+								log(el.fullTimeEquivalent);
+								
+								postChildren[k].data.fteTotal += el.fullTimeEquivalent;
+								
+								log("postChildren[k].data.fteTotal:");
+								log(postChildren[k].data.fteTotal);
+								
+								log("Added a junior post");
+								log(Orgvis.vars.postList[postID]);
+							}
+						}
 					}
 				});	 // end each loop			
 			} 
@@ -1199,7 +1197,7 @@ var Orgvis = {
 					postIn:[],
 					reportsTo:[],
 					heldBy:[],
-					salaryRange:item.salaryRange.label,
+					salaryRange:item.salaryRange ? item.salaryRange.label : 'not disclosed',
 					processed:false
 				},
 				children:[]
